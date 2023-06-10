127cc523befeef4ec121f6e37b977a8899f5e7d9
Andy Etornam Mortey <morteyandrews@gmail.com>
2023-06-10 18:53:10 +0000

    Update Query for Inserting data.sql

--- Query for Inserting data.sql
+++ Query for Inserting data.sql
@@ -108,22 +108,3 @@ INSERT INTO order_items (order_id, product_id, quantity) VALUES
 (16, 12, 2),
 (16, 13, 3);
 
-select * from customers
-
-select * from order_items
-
-select * from orders
-
-select * from products
-
-/* Questions to be answered:
-1.Which product has the highest price? Only return a single row.
-2.Which customer has made the most orders?
-3.What’s the total revenue per product?
-4.Find the day with the highest revenue.
-5.Find the first order (by date) for each customer.
-6.Find the top 3 customers who have ordered the most distinct products
-7.Which product has been bought the least in terms of quantity?
-8.What is the median order total?
-9.For each order, determine if it was ‘Expensive’ (total over 300), ‘Affordable’ (total over 100), or ‘Cheap’.
-10.Find customers who have ordered the product with the highest price.*/
